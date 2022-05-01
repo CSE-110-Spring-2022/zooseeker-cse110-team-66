f74917de4e2c6110bf5f4f061ebeb99ab80f93da
chengenl <chl084@ucsd.edu>
2022-04-30 21:12:30 -0700

    "added a mock test to compile correctly"

--- app/src/test/java/edu/ucsd/cse110/team66/zooseeker/ZooSeekerTest.java
+++ app/src/test/java/edu/ucsd/cse110/team66/zooseeker/ZooSeekerTest.java
@@ -1,11 +1,18 @@
 package edu.ucsd.cse110.team66.zooseeker;
 
+import static org.junit.Assert.assertEquals;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class ZooSeekerTest {
 
+    @Test
+    public void testTest() {
+        assertEquals(10, 5 + 5);
+    }
 
 }
