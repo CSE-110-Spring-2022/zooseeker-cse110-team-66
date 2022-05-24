4208e1c0178edd78030053d767e0e2d47fd25652
chengenl <chl084@ucsd.edu>
2022-05-24 00:42:07 -0700

    added a sharedpreference to track where the visitor is in the route and restore it if the app is killed.

--- app/src/main/java/edu/ucsd/cse110/team66/zooseeker/ExhibitDirectionsActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team66/zooseeker/ExhibitDirectionsActivity.java
@@ -2,7 +2,10 @@ package edu.ucsd.cse110.team66.zooseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ import java.util.ArrayList;
 public class ExhibitDirectionsActivity extends AppCompatActivity {
     private ArrayList<String> exhibitDirections;
     private int directionIndex;
+    int routeNum;
     TextView directionDisplay;
     Button nextDirection;
 
@@ -21,6 +25,12 @@ public class ExhibitDirectionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exhibit_directions);
+        SharedPreferences routeInfo = getSharedPreferences("routeInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = routeInfo.edit();
+
+        routeNum = routeInfo.getInt("routeNum", 0);
+        directionIndex = routeNum;
+        Log.d("directionIndex", ""+directionIndex);
         displayDirection();
         setNextDirectionButton();
     }
@@ -36,7 +46,6 @@ public class ExhibitDirectionsActivity extends AppCompatActivity {
 
     // Set up direction button
     private void setNextDirectionButton() {
-        directionIndex = 0;
         nextDirection = findViewById(R.id.next_exhibit_direction_btn);
         nextDirection.setOnClickListener(view -> nextExhibitDirection());
         ++directionIndex;
@@ -44,12 +53,18 @@ public class ExhibitDirectionsActivity extends AppCompatActivity {
 
     // Go to the next direction; return to plan route if all directions have been displayed
     private void nextExhibitDirection() {
+        SharedPreferences routeInfo = getSharedPreferences("routeInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = routeInfo.edit();
         if (directionIndex == exhibitDirections.size())
         {
+            editor.putInt("routeNum", 0);
+            editor.apply();
             finish();
             return;
         }
         directionDisplay.setText(exhibitDirections.get(directionIndex));
+        editor.putInt("routeNum", directionIndex);
+        editor.apply();
         ++directionIndex;
     }
 }
