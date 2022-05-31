78d1d078d02e0b63e81f02fbdd258ab4588f2777
anishpunaroor <anish.punaroor@gmail.com>
2022-05-30 20:35:01 -0700

    Enable/disable back

--- app/src/main/java/edu/ucsd/cse110/team66/zooseeker/ExhibitDirectionsActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team66/zooseeker/ExhibitDirectionsActivity.java
@@ -151,21 +151,15 @@ public class ExhibitDirectionsActivity extends AppCompatActivity {
     }
 
     private void backExhibitDirection() {
-        Log.d("index", String.valueOf(directionIndex));
         SharedPreferences routeInfo = getSharedPreferences("routeInfo", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = routeInfo.edit();
-        if (directionIndex < 0) {
-            editor.putInt("routeNum", 0);
-            editor.apply();
-            finish();
-            return;
-        }
-
         String currentExhibit = VisitingRoute.getExhibitToVisitAtIndex(directionIndex);
         String previousExhibit = VisitingRoute.entrance_and_exit_gate_id;
+        backDirection.setEnabled(false);
         if (directionIndex > 0) {
             previousExhibit = VisitingRoute.getExhibitToVisitAtIndex(directionIndex - 1);
             --directionIndex;
+            backDirection.setEnabled(true);
         }
         List<PlanListItem> previousDirection
                 = VisitingRoute.getPreviousExhibitDirections(currentExhibit, previousExhibit);
@@ -196,6 +190,7 @@ public class ExhibitDirectionsActivity extends AppCompatActivity {
         ++directionIndex;
         editor.putInt("routeNum", directionIndex);
         editor.apply();
+        backDirection.setEnabled(true);
         briefOrDetailedDirections();
         handleLocationChange();
     }
