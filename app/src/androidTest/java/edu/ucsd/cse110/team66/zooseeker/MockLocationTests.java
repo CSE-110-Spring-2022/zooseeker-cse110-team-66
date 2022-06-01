ed379c25ea0aa9163ebfa30b3e7435e31c18c1c8
anishpunaroor <anish.punaroor@gmail.com>
2022-05-31 21:04:47 -0700

    Some mock location tests

---
+++ app/src/androidTest/java/edu/ucsd/cse110/team66/zooseeker/MockLocationTests.java
@@ -0,0 +1,320 @@
+package edu.ucsd.cse110.team66.zooseeker;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MockLocationTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * Test that a mock location can be input by the user.
+     */
+    @Test
+    public void mockLocationCanBeInputTest() {
+        ViewInteraction cbutton = onView(
+                allOf(withId(R.id.clear_btn), withText("CLEAR"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        cbutton.perform(click());
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.exhibit_search), withContentDescription("Search Exhibit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(androidx.appcompat.R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.add_exhibit_btn), withText("ADD"),
+                        childAtPosition(
+                                allOf(withId(R.id.exhibit_item_layout),
+                                        childAtPosition(
+                                                withId(R.id.exhibit_items),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.add_exhibit_btn), withText("ADD"),
+                        childAtPosition(
+                                allOf(withId(R.id.exhibit_item_layout),
+                                        childAtPosition(
+                                                withId(R.id.exhibit_items),
+                                                4)),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.add_exhibit_btn), withText("ADD"),
+                        childAtPosition(
+                                allOf(withId(R.id.exhibit_item_layout),
+                                        childAtPosition(
+                                                withId(R.id.exhibit_items),
+                                                6)),
+                                1),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Collapse"),
+                        childAtPosition(
+                                allOf(withId(androidx.appcompat.R.id.action_bar),
+                                        childAtPosition(
+                                                withId(androidx.appcompat.R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.plan_btn), withText("Plan\n"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        3),
+                                0),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.directions_btn), withText("Directions"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.current_location_longitude), withText("-117.14936"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.current_location_longitude), withText("-117.14936"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("-121.14936"));
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.current_location_longitude), withText("-121.14936"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatEditText3.perform(closeSoftKeyboard());
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(R.id.set_mock_location), withText("Set Location"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                6),
+                        isDisplayed()));
+        materialButton6.perform(click());
+    }
+
+    /**
+     * Test that replan popup appears when user has gone off-route, and  is closer to another chosen exhibit
+     * later in the route
+     *  - In this case, user adds Capuchin Monkeys and Crocodiles
+     *   - Initially, user is closer to Crocodiles (210 feet) than Monkeys (240 feet) from Entrance
+     *     (32.73, -116.149)
+     *   - However, user's location changes to (32.741, -117.155), Monkey Trail
+     *   - Since user is closer to Capuchin Monkeys now than Crocodiles, replan dialog should appear.
+     */
+    @Test
+    public void replanPopupTest() {
+        ViewInteraction cbutton = onView(
+                allOf(withId(R.id.clear_btn), withText("CLEAR"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        cbutton.perform(click());
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.exhibit_search), withContentDescription("Search Exhibit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(androidx.appcompat.R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.add_exhibit_btn), withText("ADD"),
+                        childAtPosition(
+                                allOf(withId(R.id.exhibit_item_layout),
+                                        childAtPosition(
+                                                withId(R.id.exhibit_items),
+                                                2)),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.add_exhibit_btn), withText("ADD"),
+                        childAtPosition(
+                                allOf(withId(R.id.exhibit_item_layout),
+                                        childAtPosition(
+                                                withId(R.id.exhibit_items),
+                                                3)),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Collapse"),
+                        childAtPosition(
+                                allOf(withId(androidx.appcompat.R.id.action_bar),
+                                        childAtPosition(
+                                                withId(androidx.appcompat.R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.plan_btn), withText("Plan\n"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        3),
+                                0),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.directions_btn), withText("Directions"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.current_location_latitude), withText("32.73561"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.current_location_latitude), withText("32.73561"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("32.74161"));
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.current_location_latitude), withText("32.74161"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText3.perform(closeSoftKeyboard());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.set_mock_location), withText("Set Location"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                6),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(android.R.id.message), withText("You have gone off-route to reach the next exhibit. Do you wish to replan your route?"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class))),
+                        isDisplayed()));
+        textView.check(matches(isDisplayed()));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(android.R.id.message), withText("You have gone off-route to reach the next exhibit. Do you wish to replan your route?"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class))),
+                        isDisplayed()));
+        textView2.check(matches(withText("You have gone off-route to reach the next exhibit. Do you wish to replan your route?")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
